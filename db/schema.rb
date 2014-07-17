29f8881bd749308be5c7d3118c224c21e15be9f2
Michael Lajlev <mla@mobilethink.com>
2014-07-17 17:39:57 +0200

    tutorial done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717064408) do
+ActiveRecord::Schema.define(version: 20140717141133) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
